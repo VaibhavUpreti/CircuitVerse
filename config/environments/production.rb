e289303000ceda3f8b6073353c2b848bae76b0fa
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-08-24 02:16:16 +0530

    feat: force ssl and build arm64 images
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   end
   # Assume SSL rails 7.1
   config.assume_ssl = true
-  # config.force_ssl = true
+  config.force_ssl = true
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
